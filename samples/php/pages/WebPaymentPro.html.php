4fc5c18dacf0be16f9ca043845c2a4b60ed07eb7
Richard Peterson <Richard@cybersprocket.com>
2010-03-04 15:20:19 -0500

    Initial Commit
    This is the PayPal PHP SOAP SDK.
    
    This sourcecode was pulled from PayPal's server with no modification.
    The original code was found on the page
    
    https://cms.paypal.com/us/cgi-bin/?&cmd=_render-content&content_ID=developer/library_download_sdks
    
    Specifically, the file used was found here:
    
    https://cms.paypal.com/cms_content/US/en_US/files/developer/PP_PHP_SOAP_SDK.tar.gz

---
+++ samples/php/pages/WebPaymentPro.html.php
@@ -0,0 +1,129 @@
+<script language="JavaScript">
+	function onClick(type){
+		switch(type)
+        {
+			case 'cert':
+				document.LoginForm.apiUsername.disabled = false;
+				document.LoginForm.apiPassword.disabled = false;
+				document.LoginForm.signature.disabled = true;
+				document.LoginForm.certFile.disabled = false;
+				//document.LoginForm.privateKeyPassword.disabled = false;
+				document.LoginForm.subjectEmail.disabled = false;
+				break;
+			case 'tokens':
+				document.LoginForm.apiUsername.disabled = false;
+				document.LoginForm.apiPassword.disabled = false;
+				document.LoginForm.signature.disabled = false;
+				document.LoginForm.certFile.disabled = true;
+				//document.LoginForm.privateKeyPassword.disabled = true;
+				document.LoginForm.subjectEmail.disabled = false;
+				break;
+			case 'unipay':
+				document.LoginForm.apiUsername.disabled = true;
+				document.LoginForm.apiPassword.disabled = true;
+				document.LoginForm.signature.disabled = true;
+				document.LoginForm.certFile.disabled = true;
+				//document.LoginForm.privateKeyPassword.disabled = true;
+				document.LoginForm.subjectEmail.disabled = false;
+				break;	
+        }
+	}
+</script>
+
+<?php
+require_once 'lib/constants.inc.php';
+?>
+
+<html>
+<head>
+
+	<title>PayPal PHP SDK - API Credentials</title>
+	<link href="pages/sdk.css" rel="stylesheet" type="text/css"/>
+
+</head>
+<body alink=#0000FF vlink=#0000FF>
+<br>
+<center>
+<font size=3 color=black face=Verdana><b>Use The Default Sandbox API Profile Or Enter Your Own Profile</b></font>
+<br><br>
+<b><font color="FF0000" size=3 face=Verdana>NOTE: Production code should NEVER expose API credentials in any way! They must be <br> managed securely in your application.</font></b>
+<br><br>
+<span id=normal>
+<font size=3 color=black face=Verdana>
+To generate a Sandbox API Certificate, follow these steps: <a href="https://www.paypal.com/IntegrationCenter/ic_certificate.html#step1" target="_blank">API Certificate</a> </span > </b>
+</font>
+<br><br>
+
+<form action="WebPaymentPro.php" method="post" enctype="multipart/form-data" name="LoginForm">
+<table width="700">
+	<tr>
+		<td align="right"><span id=normalBold>API Username:</span><br/><span id=smaller>(ex: my_account_api1.paypal.com)</span></td>
+		<td>sdk-seller_api1.sdk.com</td>
+	</tr>
+	<tr>
+		<td align="right"><span id=normalBold><b>API Password:</b></span></td>
+		<td>12345678</td>
+	</tr>
+	<tr>
+		<td align="right"><span id=normalBold>Encrypted API Certificate:</span><br><span id=smaller>cert_key.pem format</span></td>
+		<td>sdk-seller_cert_key_pem.txt</td>
+	</tr>
+	<tr>
+		<td align="right"><span id=normalBold><b>Environment:</b></span></td>
+		<td><?php  echo ENVIRONMENT?></td>
+	</tr>
+	<tr>
+		<td align="right">&nbsp;</td>
+		<td align=left>
+		   <input type="hidden" name="environment" value="<?php echo ENVIRONMENT?>">
+			<input type="hidden" name="submitted" value="1">
+			<input type="submit" value="Use default account" name="DefaultButton">
+		</td>
+	</tr>
+	<tr>
+		<td colspan=3 align=center><br><b>Or enter your own profile...</b></td>
+	</tr>
+	<tr>
+		<td align="right"><span id=normalBold><b>API Credentials:</b></span>
+		<td>
+			<input type="radio" name="api-type" value="cert" onclick="onClick('cert');">Client Side SSL Certificate<br>
+			<input type="radio" name="api-type" value="tokens" checked onclick="onClick('tokens');">3 Tokens Authentication<br>
+			<input type="radio" name="api-type" value="unipay" onclick="onClick('unipay');">First Party Email (UniPay)<br>
+		</td>
+	</tr>
+	<tr>
+		<td align="right"><span id=normalBold><b>API Username:</span><br><span id=smaller>(ex: my_account_api1.paypal.com)</span></td>
+		<td><input type="text" name="apiUsername" value=""><b> Not your PayPal Email Address!</b></td>
+	</tr>
+	<tr>
+		<td align="right"><span id=normalBold><b>API Password:</span></td>
+		<td><input type="text" name="apiPassword" value=""></td>
+	</tr>
+	<tr>
+		<td align="right"><span id=normalBold>Signature:</span></td>
+		<td><input type="text" name="signature" value=""></td>
+	</tr>
+	<tr>
+		<td align="right"><span id=normalBold>Encrypted API Certificate:</span><br><span id=smaller>cert_key.pem format</span></td>
+		<td><input type="file" name="certFile" value="" disabled></td>
+	</tr>
+	<tr>
+		<td align="right"><span id=normalBold>Subject:</span></td>
+		<td><input type="text" name="subjectEmail" value=""></td>
+	</tr>
+	<tr>
+		<td align="right"><span id=normalBold><b>Environment:</span></td>
+		<td><?php echo ENVIRONMENT?></td>
+	</tr>
+	<tr>
+		<td align="right">&nbsp;</td>
+		<td align=left>
+			<input type="hidden" name="environment" value="<?php echo ENVIRONMENT?>">
+			<input type="hidden" name="submitted" value="1">
+			<input type="submit" value="Use my account" name="custom">
+		</td>
+	</tr>
+</table>
+</form>
+</body>
+</html>
